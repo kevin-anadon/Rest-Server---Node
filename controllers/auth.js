bfeb95f5c1ae60c658576c84966fc4a33f75b8c0
Kevin Anadon <mathew77200@hotmail.com>
2022-01-04 01:38:05 -0300

    Fix g-signin

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,6 @@
 const bcryptjs = require('bcryptjs');
 
-const Usuario = require('../models');
+const { Usuario } = require('../models');
 const { generarJWT } = require('../helpers/generar-jwt');
 const { googleVerify } = require('../helpers/google-verify');
 
